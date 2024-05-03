db100110e44dafe3498091de9bef6cd010c89aac
matheusghb <161875342+matheusghb@users.noreply.github.com>
2024-05-03 16:54:26 -0300

    Add files via upload

--- float.py
+++ float.py
@@ -6,8 +6,14 @@ class MinhaApp(App):
     def build(self):
 
         layout_f = FloatLayout()
-        btn = Button(text="Clique aqui", size_hint=(None, None), size=(100,50), pos_hint={'x': 0.5, 'y': 0.5})
+        btn = Button(text="Botão 1", size_hint=(None, None), size=(100,50), pos_hint={'x': 0.1, 'y': 0.8})
+        btn2 = Button(text="Botão 2", size_hint=(None, None), size=(100,50), pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        btn3 = Button(text="Botão 1", size_hint=(None, None), size=(100,50), pos_hint={'right': 0.9, 'y': 0.1})
         layout_f.add_widget(btn)
+        layout_f.add_widget(btn2)
+        layout_f.add_widget(btn3)
+
+        return layout_f
 
 if __name__ == "__main__":
     MinhaApp().run()
